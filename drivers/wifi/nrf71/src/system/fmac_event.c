7bb05f9dcdd9d8ea7f95bb6847f3d3b86c12abf1
Ajay Parida <ajay.parida@nordicsemi.no>
2026-06-22 17:31:33 +0530

    FTM feature support
    FTM feature support.
    
    Signed-off-by: Ajay Parida <ajay.parida@nordicsemi.no>

--- drivers/wifi/nrf71/src/system/fmac_event.c
+++ drivers/wifi/nrf71/src/system/fmac_event.c
@@ -1125,6 +1125,17 @@ static enum nrf_wifi_status umac_event_ctrl_process(struct nrf_wifi_fmac_dev_ctx
 					      umac_hdr->cmd_evnt);
 		}
 		break;
+	case NRF_WIFI_UMAC_EVENT_PEER_MEAS_RESULTS:
+		if (callbk_fns->ftm_peer_result_callbk_fn) {
+			callbk_fns->ftm_peer_result_callbk_fn(vif_ctx->os_vif_ctx,
+							      event_data,
+							      event_len);
+		} else {
+			LOG_ERR("%s: No callback registered for event %d",
+					      __func__,
+					      umac_hdr->cmd_evnt);
+		}
+		break;
 	case NRF_WIFI_UMAC_EVENT_BEACON_HINT:
 	case NRF_WIFI_UMAC_EVENT_CONNECT:
 	case NRF_WIFI_UMAC_EVENT_DISCONNECT:
